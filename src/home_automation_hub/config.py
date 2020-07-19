40f75def516a13e814fdb3346154cbde45a0fe6b
levidavis <47396371+levidavis@users.noreply.github.com>
2020-07-19 19:42:01 +0100

    Update config.py

--- src/home_automation_hub/config.py
+++ src/home_automation_hub/config.py
@@ -1,13 +1,6 @@
 from .config_store import ConfigStore
-import home_automation_heating
-import home_automation_influxdb_logger
 
 config = ConfigStore()
-config.register_module(home_automation_heating, "heating", "Central Heating",
-        "heating", fontawesome_icon_class="thermometer-half")
-config.register_module(home_automation_influxdb_logger, "influxdb_logger",
-        "InfluxDB Logger", "influxdb_logger",
-        fontawesome_icon_class="database")
 
 config.set_mqtt_broker("mqtt", 1883)
 
